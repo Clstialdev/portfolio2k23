88de3968873763a2043558700756e80bc8733185
clstialdev <rusty75001@gmail.com>
2024-09-24 18:33:22 +0200

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
